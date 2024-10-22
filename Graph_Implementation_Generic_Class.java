108c85d8d09819a4bccfec20b43416ab50d99087
ANUJ KUMAR <kumar.anuj.28082004@gmail.com>
2024-10-22 15:43:06 +0530

    SUCCESS

--- Graph_Implementation_Generic_Class.java
+++ Graph_Implementation_Generic_Class.java
@@ -126,6 +126,22 @@ public class Graph_Implementation_Generic_Class<V>{
             }
         }
     }
+    public void DFS(V vertex1){
+        Stack<Node> q=new Stack<>();
+        HashSet<V> visited=new HashSet<>();
+        q.add(new Node(vertex1,graph.get(vertex1)));
+        visited.add(vertex1);
+        while(!q.isEmpty()){
+            Node curr=q.pop();
+            System.out.print(curr.key+" ");
+            for(V adj:curr.neigh.keySet()){
+                if(!visited.contains(adj)){
+                    q.add(new Node(adj,graph.get(adj)));
+                    visited.add(adj);
+                }
+            }
+        }
+    }
     public static void main(String args[]){
         Graph_Implementation_Generic_Class<Integer> graph=new Graph_Implementation_Generic_Class<>();
         graph.AddEdge(1, 2, 0);
@@ -138,16 +154,34 @@ public class Graph_Implementation_Generic_Class<V>{
         graph.AddEdge(1, 6, 0);
         System.out.println(graph.VertexCount());
         System.out.println(graph.EdgeCount());
+        System.out.println();
+        System.out.println("All Edge Print");
         graph.AllEdgePrint();
+        System.err.println();
+        System.out.println("IS PATH BETWEEN GIVING TWO NODE");
         System.out.println(graph.IsPath(1, 5,new HashSet<>()));
         System.out.println(graph.IsPath(1, 7,new HashSet<>()));
+        System.out.println();
+        System.err.println("FIRST POSSIBLE PATH");
         graph.PrintPath(1, 6, new HashSet<>(),"");
+        System.out.println();
+        System.out.println("All Possible Path Between Vertices");
         graph.PrintAllPath(1, 4, new HashSet<>(),"");
         graph.PrintAllPath(1, 6, new HashSet<>(),"");
         graph.PrintAllPath(1, 5, new HashSet<>(),"");
+        System.out.println();
+        System.out.println("Path Count Between Two Node");
         System.out.println(graph.PathCount(1, 4, new HashSet<>()));
         System.out.println(graph.PathCount(1, 6, new HashSet<>()));
         System.out.println(graph.PathCount(1, 5, new HashSet<>()));
+        System.out.println("BFS");
         graph.BFS(1);
+        System.out.println();
+        graph.BFS(4);
+        System.out.println();
+        System.out.println("DFS");
+        graph.DFS(1);
+        System.out.println();
+        graph.DFS(4);
     }
 }
