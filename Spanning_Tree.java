02ce90d3169606c65f3716edfafc03ba017ecaff
ANUJ KUMAR <kumar.anuj.28082004@gmail.com>
2024-11-06 12:40:48 +0530

    Done

---
+++ Spanning_Tree.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Spanning_Tree<V> extends Graph_Implementation_Generic_Class<V> {
+    public Spanning_Tree() {
+        super();
+    }
+    public class Node implements Comparable<Node> {
+        V key;
+        V value;
+        int weight;
+        public Node(V key, V value, int weight) {
+            this.key = key;
+            this.value = value;
+            this.weight = weight;
+        }
+        public int compareTo(Node b) {
+            return this.weight - b.weight;
+        }
+    }
+    public void MST(V start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        HashSet<V> visited = new HashSet<>();
+        int ans = 0;
+        pq.add(new Node(start, start, 0));
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
+            if (visited.contains(curr.value)) {
+                continue;
+            }
+            visited.add(curr.value);
+            ans += curr.weight;
+            System.out.println("Edge: " + curr.key + " - " + curr.value + " with weight: " + curr.weight);
+            if (graph.containsKey(curr.value)) {
+                for (V neigh : graph.get(curr.value).keySet()) {
+                    if (!visited.contains(neigh)) {
+                        pq.add(new Node(curr.value, neigh, graph.get(curr.value).get(neigh)));
+                    }
+                }
+            }
+        }
+        System.out.println("Total weight of MST: " + ans);
+    }
+    public static void main(String[] args) {
+        Spanning_Tree<Integer> tree = new Spanning_Tree<>();
+        tree.AddEdge(1, 2, 10);
+        tree.AddEdge(1, 4, 20);
+        tree.AddEdge(2, 3, 15);
+        tree.AddEdge(3, 4, 25);
+        tree.MST(1);
+    }
+}
