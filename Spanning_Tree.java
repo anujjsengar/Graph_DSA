45082a92055705ce042c5c063e39e46d76ebfeea
ANUJ KUMAR <kumar.anuj.28082004@gmail.com>
2024-11-07 18:17:15 +0530

    Done

--- Spanning_Tree.java
+++ Spanning_Tree.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Spanning_Tree<V> extends Graph_Implementation_Generic_Class<V> {
     public Spanning_Tree() {
         super();
@@ -40,12 +41,36 @@ public class Spanning_Tree<V> extends Graph_Implementation_Generic_Class<V> {
         }
         System.out.println("Total weight of MST: " + ans);
     }
+    public void shortest_path(V start){
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        HashSet<V> visited=new HashSet<>();
+        pq.add(new Node(start,start,0));
+        while(!pq.isEmpty()){
+            Node curr=pq.poll();
+            if(visited.contains(curr.value)){
+                continue;
+            }
+            visited.add(curr.value);
+            System.out.print(curr.value+" --> ");
+            if(graph.containsKey(curr.value)){
+                for(V neigh:graph.get(curr.value).keySet()){
+                    if(!visited.contains(neigh)){
+                        pq.add(new Node(curr.value,neigh,graph.get(curr.value).get(neigh)));
+                    }
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         Spanning_Tree<Integer> tree = new Spanning_Tree<>();
         tree.AddEdge(1, 2, 10);
         tree.AddEdge(1, 4, 20);
-        tree.AddEdge(2, 3, 15);
-        tree.AddEdge(3, 4, 25);
+        tree.AddEdge(1, 3, 25);
+        tree.AddEdge(2, 4, 30);
+        tree.AddEdge(2, 3, 27);
+        tree.AddEdge(3, 4, 15);
         tree.MST(1);
+        System.out.println("Shortest Path from a Vertex");
+        tree.shortest_path(2);
     }
 }
